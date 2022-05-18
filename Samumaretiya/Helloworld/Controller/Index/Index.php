d061ef981e8489fab1c3941074af5d72220b9cc2
sam <sam@kammalou.com>
2022-05-18 20:51:24 +0530

    initial source code

---
+++ Samumaretiya/Helloworld/Controller/Index/Index.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Samumaretiya\Helloworld\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+		echo "Hello how are you";
+		exit;
+    }
+}
